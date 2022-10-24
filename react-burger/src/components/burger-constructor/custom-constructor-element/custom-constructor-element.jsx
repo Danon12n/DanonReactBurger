bc8ae6ba8b989ce382da158dc683687528489a27
Даниил Нечитайло <masterwars22@yandex.ru>
2022-10-24 15:24:02 +0300

    upd
    небольшой рефакторинг
    убрал комментарии, логи, не использующиеся переменные

--- react-burger/src/components/burger-constructor/custom-constructor-element/custom-constructor-element.jsx
+++ react-burger/src/components/burger-constructor/custom-constructor-element/custom-constructor-element.jsx
@@ -22,24 +22,8 @@ export default function CustomConstructorElement({
 }) {
     const dispatch = useDispatch();
 
-    //---------------------------------
-    // const { filling } = useSelector(
-    //     (store) => store.main.ingredientsInConstructor
-    // );
-
     const ref = useRef();
     const moveCard = useCallback((dragIndex, hoverIndex) => {
-        // const new_filling = filling.map((el) => {
-        //     return el;
-        // });
-        // const dragCard = new_filling[dragIndex];
-        // console.log(dragCard);
-        // console.log(new_filling);
-        // new_filling.splice(dragIndex, 1);
-        // console.log(new_filling);
-        // new_filling.splice(hoverIndex, 0, dragCard);
-        // console.log(new_filling);
-
         dispatch({
             type: SWAP_CONSTRUCTOR_INGREDIENTS,
             dragIndex: dragIndex,
@@ -60,50 +44,39 @@ export default function CustomConstructorElement({
             }
             const dragIndex = item.index;
             const hoverIndex = index;
-            // Don't replace items with themselves
+
             if (dragIndex === hoverIndex) {
                 return;
             }
-            // Determine rectangle on screen
+
             const hoverBoundingRect = ref.current?.getBoundingClientRect();
-            // Get vertical middle
+
             const hoverMiddleY =
                 (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
-            // Determine mouse position
+
             const clientOffset = monitor.getClientOffset();
-            // Get pixels to the top
+
             const hoverClientY = clientOffset.y - hoverBoundingRect.top;
-            // Only perform the move when the mouse has crossed half of the items height
-            // When dragging downwards, only move when the cursor is below 50%
-            // When dragging upwards, only move when the cursor is above 50%
-            // Dragging downwards
+
             if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
                 return;
             }
-            // Dragging upwards
+
             if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
                 return;
             }
-            // Time to actually perform the action
+
             moveCard(dragIndex, hoverIndex);
-            // Note: we're mutating the monitor item here!
-            // Generally it's better to avoid mutations,
-            // but it's good here for the sake of performance
-            // to avoid expensive index searches.
             item.index = hoverIndex;
         },
     });
-    const [{ isDragging }, drag] = useDrag({
+    const [{}, drag] = useDrag({
         type: "sort",
         item: () => {
             return { index };
         },
-        collect: (monitor) => ({
-            isDragging: monitor.isDragging(),
-        }),
     });
     drag(drop(ref));
-    //-----------------------------------
 
     return isBun ? (
         <ConstructorElement
